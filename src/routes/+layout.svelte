7ea57428cb84eb518dc944a9e37e10a7ebbdf922
Ludivine Le Lan <lelanludivine@gmail.com>
2025-10-01 15:04:14 +0200

    forgot password page + layout + footer fixed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,8 @@
 	import favicon from '$lib/assets/favicon.png';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-    import '../reset.css';
+	import '../reset.css';
 	import '../app.css';
-
 
 	let { children } = $props();
 </script>
@@ -15,8 +14,30 @@
 
 <div class="layout">
 	<Header></Header>
-	
-	{@render children?.()}
-	
+
+	<main class="main-content">
+		{@render children?.()}
+	</main>
+
 	<Footer></Footer>
 </div>
+
+<style>
+	html,
+	body {
+		height: 100%;
+		margin: 0;
+	}
+
+	.layout {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.main-content {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+	}
+</style>
